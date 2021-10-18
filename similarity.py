26d0e717d5ec3ec51c92fceacb480814b73e1bc4
wschooltink <schooltink.willem@gmail.com>
2021-10-18 14:56:29 +0200

    Optimisation

--- similarity.py
+++ similarity.py
@@ -2,6 +2,13 @@ import re
 
 
 def similarity_score(phoneme1, phoneme2):
+    '''
+
+    :param phoneme1:
+    :param phoneme2:
+    :return:
+    '''
+
     ptuple1 = to_tuple(phoneme1)
     ptuple2 = to_tuple(phoneme2)
     print(ptuple1)
@@ -9,19 +16,10 @@ def similarity_score(phoneme1, phoneme2):
 
     if phoneme1 == phoneme2:
         return 1
-    #TODO: Add similarity scores example:
-    if ptuple1[0] == "AA":
-        if ptuple2[0] == "AE":
-            score = .8
-        elif ptuple2[0] == "AH":
-            score = .5
-        elif ptuple2[0] == "AX":
-            score = .7
-        elif ptuple2[0] == "EH":
-            score = .7
-        else:
-            score = 0
-    else:
+
+    phones = sorted([ptuple1[0], ptuple2[0]])
+    score = similarity_dict[phones[0], phones[1]]
+    if score is None:
         score = 0
 
     # If the phoneme has stress on it adjust score to difference in stress
@@ -33,7 +31,24 @@ def similarity_score(phoneme1, phoneme2):
 
 
 def to_tuple(phoneme):
+    '''
+    Split phonemes into their phonetic description and possible
+    :param phoneme:
+    :return:
+    '''
     match = re.match(re.compile('([A-Z]+)([0-2])?'), phoneme)
     letters = match.group(1)
     addition = match.group(2)
     return [letters, addition]
+
+
+similarity_dict = {
+    "AA": {
+        "AE": .9,
+        "AH": .9,
+    },
+    "AE": {
+
+    },
+    # TODO...
+}
